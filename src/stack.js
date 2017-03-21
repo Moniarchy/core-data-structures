efc509f6f1a2efe3072417de01a4264b5133dac9
Monica Williams <monicaestellaw@gmail.com>
2017-03-21 11:31:51 -0700

    isEmpty implementation passes

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,35 @@
 'use strict'
 
 export default class Stack {
-  // your code here
+  constructor() {
+    this.node = { data: '', next: ''}
+    this.top = null
+    //a node is an object
+    //it has data and a pointer to the next node
+    //if it is the head it has that info as well
+    //if it is the tail it does not have a pointer to the next node
+  }
+
+  push( ) {
+    //update this.top
+  }
+
+  pop() {
+    //return this.top
+    //then update this.top
+  }
+
+  peek() {
+
+  }
+
+  length() {
+
+  }
+
+  isEmpty() {
+    if ( this.top === null ) {
+      return true
+    }
+  }
 }
